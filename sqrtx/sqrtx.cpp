9c7edc9b5b32bdef9f1495eda34ee41b15c3b556
bbainwar <59973136+bbainwar@users.noreply.github.com>
2021-08-18 15:53:29 +0530

    Time: 60 ms (5.04%), Space: 5.8 MB (71.74%) - LeetHub

--- sqrtx/sqrtx.cpp
+++ sqrtx/sqrtx.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        for(int i=46340; i>=0; i--){
-            if(i*i<=x)
-                return i;
+        if(x==0){return 0;}
+        double low=0, high=x;
+        double mid=(low+high)/2;
+        int count=10000;
+        while(count--){
+            mid=(low+high)/2;
+            low=mid; high=x/mid;
         }
-        return -1;
+        return (int)mid;
     }
 };
