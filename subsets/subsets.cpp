3714379e97d91ddba3b3569a5cec8afde3024a3f
bbainwar <59973136+bbainwar@users.noreply.github.com>
2021-08-31 21:02:00 +0530

    Time: 4 ms (59.03%), Space: 7.4 MB (37.65%) - LeetHub

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void solution(vector<int> nums, int index, vector<int> tillNow){
-        if(index==nums.size()){
-            ans.push_back(tillNow);
-            return;
+    void solution(vector<int> nums, vector<int> prev, int ind, vector<vector<int>>& ans){
+        if(ind==nums.size()){return;}
+        
+        for(int i=ind; i<nums.size(); i++){
+            prev.push_back(nums[i]);
+            ans.push_back(prev);
+            solution(nums, prev, i+1, ans);
+            prev.pop_back();
         }
-        solution(nums, index+1, tillNow);
-        tillNow.push_back(nums[index]);
-        solution(nums, index+1, tillNow);
+        
         return;
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        solution(nums, 0, {});
+        vector<vector<int>> ans;
+        ans.push_back({});
+        solution(nums, {}, 0, ans);
         return ans;
     }
 };
