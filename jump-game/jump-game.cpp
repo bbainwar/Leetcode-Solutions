4c67c8e924faadf5eb527b5b3c3e3ec403114816
bbainwar <59973136+bbainwar@users.noreply.github.com>
2021-09-02 23:11:35 +0530

    Time: 1072 ms (5.01%), Space: 48.7 MB (16.14%) - LeetHub

---
+++ jump-game/jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<bool> dp(n, false);
+        dp[n-1]=true;
+        for(int i=n-2; i>=0; i--){
+            for(int j=i+1; j<=i+nums[i]&&j<nums.size(); j++){
+                if(dp[j]==true){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+};
