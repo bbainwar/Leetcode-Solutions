8329dcef671b00e7260679bcc6b35cb049ef2fb7
bbainwar <59973136+bbainwar@users.noreply.github.com>
2021-09-04 02:07:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (63.47%) - LeetHub

---
+++ valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low=1, high=46340, mid;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(mid*mid==num){
+                return true;
+            }
+            else if(mid*mid<num){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+};
