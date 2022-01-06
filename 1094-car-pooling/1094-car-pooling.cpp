b0f5f4f3444bcc84d52ac254dd49f5243a2c341f
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-01-07 00:07:28 +0530

    Time: 21 ms (24.45%), Space: 10.5 MB (29.36%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    static bool comp(vector<int> v1, vector<int> v2){
-        return v1[1]<v2[1];
-    }
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(), trips.end(), comp);
-        for(int i=0; i<trips.size(); i++){
-            swap(trips[i][0], trips[i][2]);
+        vector<pair<int, int>> arr;
+        for(auto a: trips){
+            arr.push_back({a[1], a[0]});
+            arr.push_back({a[2], -a[0]});
         }
-        
-        int presReq=0;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>> > endTime;
-        for(int i=0; i<trips.size(); i++){
-            while(endTime.empty()==false && endTime.top()[0]<=trips[i][1]){
-                presReq-=endTime.top()[2];
-                endTime.pop();
-            }
-            endTime.push(trips[i]);
-            presReq+=trips[i][2];
-            if(presReq>capacity){
+        sort(arr.begin(), arr.end());
+        int curr=0;
+        for(int i=0; i<arr.size(); i++){
+            curr+=arr[i].second;
+            if(curr>capacity){
                 return false;
             }
         }
