fb389955f17cc71935ef07f6713e8f49b369eebf
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-01-08 11:04:44 +0530

    Time: 396 ms (13.01%), Space: 171.4 MB (11.79%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,49 +1,35 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    int dp[16][16];
-    bool checkPalin(string str, int start, int end){
-        if(dp[start][end]!=-1){
-            return dp[start][end];
-        }
-        while(start<end){
-            if(str[start]!=str[end]){
-                return dp[start][end]=0;
-            }
+    bool isPalin(string str, int start, int end){
+        while(start<=end){
+            if(str[start]!=str[end])
+                return false;
             start++; end--;
         }
-        return dp[start][end]=1;
+        return true;
     }
-    void print(vector<string> arr){
-        for(auto s: arr)
-            cout<<s<<" ";
-        cout<<"\n";
-    }
-    
-    void generate(string str, int start, vector<string> tillNow){
+
+    vector<vector<string>> ans;
+    void palPart(string str, int start, vector<string> tillNow){
         if(start>=str.size()){
-            // print(tillNow);
             ans.push_back(tillNow);
+            return;
         }
-        int sz=start;
-        while(true){
-            if(sz<str.size()){
-                if(checkPalin(str, start, sz)){
-                    tillNow.push_back(str.substr(start, sz-start+1));
-                    generate(str, sz+1, tillNow);
-                    tillNow.pop_back();
-                }
-                sz++;
-            }
-            else{
-                break;
+
+        for(int i=start; i<str.size(); i++){
+            if(isPalin(str, start, i)==true){
+                tillNow.push_back(str.substr(start, i-start+1));
+                palPart(str, i+1, tillNow);
+                tillNow.pop_back();
             }
         }
+
     }
-    
+
+
     vector<vector<string>> partition(string s) {
-        memset(dp, -1, sizeof(dp));
-        generate(s, 0, {});
+        vector<string> tillNow;
+        palPart(s, 0, tillNow);
         return ans;
     }
 };
