8c2ec01837b924ae7b36efeec3845d75092f3ee9
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-01-11 10:57:30 +0530

    Time: 3 ms (68.26%), Space: 6.3 MB (59.77%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -2,56 +2,20 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         string ans="";
-        char carry='0';
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
-        int i=0, j=0;
-        int sa=a.size(), sb=b.size();
-        if(sa>sb){
-            for(int ind=0; ind<(sa-sb); ind++){
-                b+='0';
+        int i=a.size()-1, j=b.size()-1, carry=0;
+        while(i>=0 || j>=0 || carry){
+            if(i>=0){
+                carry+=a[i]-'0';
+                i--;
             }
+            if(j>=0){
+                carry+=b[j]-'0';
+                j--;
+            }
+            ans+=('0'+carry%2);
+            carry/=2;
         }
-        else{
-            for(int ind=0; ind<(sb-sa); ind++){
-                a+='0';
-            }
-        }
-        cout<<a<<"\n"<<b<<"\n";
-        int count=0;
-        while(i<a.size() && j<b.size()){
-            count++;
-            if(carry=='0'){
-                if((a[i]=='0'&&b[j]=='1')||(a[i]=='1'&&b[j]=='0')){
-                    ans+='1';
-                }
-                else if(a[i]=='1'&&b[j]=='1'){
-                    // cout<<"here1";
-                    carry='1';
-                    ans+='0';
-                }
-                else{
-                    ans+='0';
-                }
-            }
-            else{
-                if((a[i]=='0'&&b[j]=='1')||(a[i]=='1'&&b[j]=='0')){
-                    cout<<"here1";
-                    ans+='0';
-                }
-                else if(a[i]=='1'&&b[j]=='1'){
-                    ans+='1';
-                }
-                else{
-                    ans+='1';
-                    carry='0';
-                }
-            }
-            i++; j++;
-        }
-        cout<<ans<<endl<<count;
-        if(carry=='1'){
-            cout<<"here";
+        if(carry==1){
             ans+='1';
         }
         reverse(ans.begin(), ans.end());
