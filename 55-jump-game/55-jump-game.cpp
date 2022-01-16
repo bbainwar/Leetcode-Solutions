70c32311f5d4355b3f53f2325b3d9e08318f9e7e
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-01-16 12:38:15 +0530

    Time: 110 ms (33.14%), Space: 48.3 MB (49.34%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach=0, n=nums.size();
+        for(int i=0; i<n; i++){
+            if(i>maxReach) return false;
+            maxReach=max(maxReach, i+nums[i]);
+        }
+        return true;
+    }
+};
