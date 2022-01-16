8a59069c2f238da8f80f1da224ef52bf58a8b7f5
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-01-16 11:15:03 +0530

    Time: 492 ms (16.58%), Space: 17.8 MB (6.56%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int min(int a, int b){
+        return (a<b)?a:b;
+    }
+    int solution(vector<int>& nums, vector<int>& dp, int start){
+        if(start>=nums.size()-1){
+            return 0;
+        }
+        if(dp[start]!=-1) return dp[start];
+        int ans=100005;
+        for(int i=start+1; i<=min(start+nums[start], nums.size()-1); i++){
+            if(i==nums.size()-1){
+                return dp[start]=1;
+            }
+            if(nums[i]!=0)
+                ans=min(ans, 1+solution(nums, dp, i));
+        }
+        return dp[start]=ans;
+    }
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        return solution(nums, dp, 0);
+    }
+};
