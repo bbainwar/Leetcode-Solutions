69ef026135f82c64da720ac6be1cc3bc5c1245cf
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-01-20 15:33:16 +0530

    Time: 32 ms (98.47%), Space: 18.9 MB (16.35%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    bool check(vector<int>& piles, int h, long k){
-        long hours=0;
+    bool check(vector<int>& piles, int h, int k){
+        int hours=0;
         for(int i: piles){
             hours+=i/k;
             if(i%k!=0) hours++;
@@ -9,7 +9,7 @@ public:
         return hours<=h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        long low=1, high=1e12, mid;
+        int low=1, high=INT_MAX, mid;
         while(low<high){
             mid=low+(high-low)/2;
             if(check(piles, h, mid)){
