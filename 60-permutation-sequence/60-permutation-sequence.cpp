00155017cf32994fbba3eef3afaf179f89f64488
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-03-01 10:41:00 +0530

    Time: 5 ms (37.31%), Space: 6.3 MB (16.85%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -6,36 +6,16 @@ public:
         return ans;
     }
     string getPermutation(int n, int k) {
-        if(k==fac(n)){
-            string ans="";
-            for(int i=n; i>0; i--){
-                ans+=(i+'0');
-            }
-            return ans;
-        }
         queue<int> seq;
         for(int i=n; i>0; i--){
-            // cout<<k<<" ";
-            if(k==0){
-                seq.push(1);
-            }
-            else{
-                int tf=fac(i-1);
-                seq.push(ceil(((double)k)/tf));
-                int rem=k%tf;
-                if(rem==0)
-                    k=tf;
-                else
-                    k=k%tf;
-            }
+            int tf=fac(i-1);
+            seq.push(ceil(((double)k)/tf));
+            int rem=k%tf;
+            if(rem==0)
+                k=tf;
+            else
+                k=k%tf;
         }
-        // cout<<endl;
-        // queue<int> temp=seq;
-        // while(!temp.empty()){
-        //     cout<<temp.front()<<" ";
-        //     temp.pop();
-        // }
-        // cout<<endl;
         vector<int> nums(n);
         iota(nums.begin(), nums.end(), 1);
         string ans="";
