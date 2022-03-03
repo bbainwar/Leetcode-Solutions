52839433f2d87755c583da20085a7c2b9f0ab34e
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-03-03 11:28:59 +0530

    Time: 3 ms (66.63%), Space: 7.3 MB (88.02%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -3,22 +3,17 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int totalSlices=0, n=nums.size();
         if(n<3) return 0;
-        int presDiff=nums[1]-nums[0], lSize=2;
+        int presDiff=nums[1]-nums[0], lSize=0;
         for(int i=2; i<n; i++){
             if(nums[i]-nums[i-1]==presDiff){
                 lSize++;
+                totalSlices+=lSize;
             }
             else{
-                if(lSize>=3)
-                    totalSlices+=((lSize-1)*(lSize-2))/2;
-        
                 presDiff=nums[i]-nums[i-1];
-                lSize=2;
+                lSize=0;
             }
         }
-        if(lSize>=3)
-            totalSlices+=((lSize-1)*(lSize-2))/2;
-        
         return totalSlices;
     }
 };
