600b3e50fdc7b853765438a011b2ed101adc1602
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-05-12 01:46:36 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (85.71%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    int dp[55][6];
-    int sol(int l, int s){
-        if(s==1) return l;
-        if(l==0 || s<1) return 0;
-        if(dp[s][l]!=-1) return dp[s][l];
-        return dp[s][l]=sol(l, s-1)+sol(l-1, s);
-    }
     int countVowelStrings(int n) {
-        memset(dp, -1, sizeof(dp));
-        return sol(5, n);
+        int dp[6][n+1];
+        
+        for(int i=0; i<6; i++){
+            dp[i][0]=0;
+            dp[i][1]=i;
+        } 
+        for(int i=0; i<n+1; i++){
+            dp[0][i]=0;
+        } 
+        
+        for(int i=1; i<6; i++){
+            for(int j=2; j<n+1; j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        
+        return dp[5][n];
     }
 };
