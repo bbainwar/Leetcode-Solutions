f6b55e16ad2895801c640452a9fac3dc19ba54a6
bbainwar <59973136+bbainwar@users.noreply.github.com>
2022-08-14 23:26:06 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (16.89%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int dp[105];
+    
+    int conv(string s){
+        if(s.size()==1){
+            return s[0]-'0';
+        }
+        else{
+            int ans=0;
+            ans+=(s[0]-'0')*10;
+            ans+=(s[1]-'0');
+            return ans;
+        }
+    }
+    
+    int decode(string s, int b){
+        if(b>s.size() || s[b]=='0'){
+            return 0;
+        }
+        if(dp[b]!=-1) return dp[b];
+        if(b==s.size()){
+            return 1;
+        }
+        
+        int ans=0;
+        ans+=decode(s, b+1);
+        int temp=conv(s.substr(b, 2));
+        if(temp>=10 && temp<=26){
+            // cout<<temp;
+            ans+=decode(s, b+2);
+        }
+        
+        dp[b]=ans;
+        return ans;
+    }
+    
+    int numDecodings(string s) {
+        memset(dp, -1, sizeof(dp));
+        return decode(s, 0);
+    }
+};
